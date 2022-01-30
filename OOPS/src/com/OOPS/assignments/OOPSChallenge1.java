e69877fc4f9f9090b18a81c015caa16b60d541a7
sayan <subhosen16@gmail.com>
2022-01-30 16:32:30 +0530

    Committing OOPS assignments

---
+++ OOPS/src/com/OOPS/assignments/OOPSChallenge1.java
@@ -0,0 +1,14 @@
+package com.OOPS.assignments;
+
+public class OOPSChallenge1 {
+
+	void printString() {
+		System.out.println("Hello Great Learning");
+	}
+
+	public static void main(String[] args) {
+		OOPSChallenge1 obj = new OOPSChallenge1();
+		obj.printString();
+	}
+
+}
