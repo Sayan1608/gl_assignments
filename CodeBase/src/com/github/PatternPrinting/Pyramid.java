6b0baee842a1ab52090fbb45d5970b768e2ab38c
sayan <subhosen16@gmail.com>
2022-02-02 18:33:27 +0530

    Adding 2nd Pyramid pattern to Pyramid.java file

--- CodeBase/src/com/github/PatternPrinting/Pyramid.java
+++ CodeBase/src/com/github/PatternPrinting/Pyramid.java
@@ -6,13 +6,15 @@ public class Pyramid {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter the height of pyramid::");
+		System.out.print("Enter the height of pyramid2::");
 		int height = in.nextInt();
 		System.out.println("\n");
-		pyramid(height);
+//		pyramid1(height);
+		pyramid2(height);
+		in.close();
 	}
 
-	public static void pyramid(int h) {
+	public static void pyramid1(int h) {
 		for (int i = 1; i <= h; i++) {
 			for (int k = h; k > i; k--) {
 				System.out.print(" ");
@@ -20,6 +22,19 @@ public class Pyramid {
 
 			for (int j = 1; j <= i; j++) {
 				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void pyramid2(int h) {
+		for (int i = 1; i <= h; i++) {
+			for (int k = h; k > i; k--) {
+				System.out.print(" ");
+			}
+
+			for (int j = 1; j <= (2 * i - 1); j++) {
+				System.out.print("*");
 			}
 			System.out.println();
 		}
