3c810cc98fa6648c01c630c84ac604b481f13563
sayan <subhosen16@gmail.com>
2022-02-20 13:23:05 +0530

    Committing AccessModifiers Challenge to Assignments

---
+++ Assignments/src/com/greatlearning/corejava/Health.java
@@ -0,0 +1,7 @@
+package com.greatlearning.corejava;
+
+public class Health {
+	protected int age = 25;
+	protected float weight = 78.5f;
+	protected float bmi = 21.456f;
+}
---
+++ Assignments/src/com/greatlearning/corejava/HealthDriver.java
@@ -0,0 +1,12 @@
+package com.greatlearning.corejava;
+
+public class HealthDriver {
+
+	public static void main(String[] args) {
+		Health h1 = new Health();
+		
+		System.out.println("age: "+h1.age +" "+ "weight: "+ h1.weight+" "+"bmi: "+h1.bmi);
+		
+	}
+
+}
