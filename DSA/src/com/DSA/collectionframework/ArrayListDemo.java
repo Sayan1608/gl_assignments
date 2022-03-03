903eca3f66582809d1c129d6027782035fb0524d
sayan <subhosen16@gmail.com>
2022-03-03 11:01:25 +0530

    Committing the changes

--- DSA/src/com/DSA/collectionframework/ArrayListDemo.java
+++ DSA/src/com/DSA/collectionframework/ArrayListDemo.java
@@ -45,6 +45,8 @@ public class ArrayListDemo {
 		System.out.println("list1: "+list1);
 		System.out.println("list2: "+list2);
 		
+		System.out.println("Element at 3rd index: "+list2.get(3));
+		
 	}
 
 }
